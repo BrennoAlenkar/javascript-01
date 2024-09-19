2c236fa90ebea8cb0f0c9290254434c3698b6814
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-09-19 06:39:39 -0300

    incremento

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let numero = 0;
+
+const incrimentar = () => {
+    document.querySelector('.teste').innerHTML = numero;
+    numero++;
+}
+
+
+function tirar() {
+    document.querySelector('.teste').innerHTML = numero;
+    numero--;
+}
