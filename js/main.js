bd31b839281e0725f1d3d814eb32bf679cee6914
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-10-09 11:59:29 -0300

    API

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
-const segundos = 1000;
-let cronometro = 0;
+function buscarCEP() {
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', 'https://viacep.com.br/ws/74423460/json/');
+    ajax.send();
 
-function iniciar() {
-    setInterval(() => {
-        cronometro += 1; 
-        document.querySelector('.teste').innerHTML = cronometro; 
-    }, segundos);
+    ajax.onload = function () {
+        
+        document.querySelector('.teste').innerHTML = this.responseText;
+
+        const OBJ = JSON.parse(this.responseText);
+
+        let cidade = OBJ.localidade;
+        let uf = OBJ.uf;
+        let cep = OBJ.cep;
+        let complemento = OBJ.complemento;
+
+        document.querySelector('.teste').innerHTML = `
+        Cidade: ${cidade},
+        Região: ${uf},
+        CEP: ${cep},
+        Referência: ${complemento},
+
+        `
+    }
+
 }
 
-iniciar();
+buscarCEP()
+
+
+
